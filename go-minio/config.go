ed590474078c531ee18d6db57362ffa01c6470d2
姚吉日 <gif@live.cn>
2024-05-15 20:10:45 +0800

    add minio client dir

--- go-minio/config.go
+++ go-minio/config.go
@@ -5,6 +5,7 @@ type Config struct {
 	AccessKeySecret string `json:",optional" yaml:"AccessKeySecret"`
 	Endpoint        string `json:",optional" yaml:"Endpoint"`
 	Bucket          string `json:",optional" yaml:"Bucket"`
+	Dir             string `json:",optional" yaml:"Dir"`
 	Domain          string `json:",optional" yaml:"Domain,optional"`
 	UseSSL          bool   `json:",optional" yaml:"UseSSL"`
 }
