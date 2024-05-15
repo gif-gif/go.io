c68b83e0f99155fb1a585eeb738e7631232a7447
姚吉日 <gif@live.cn>
2024-05-15 18:24:59 +0800

    config

--- go-minio/config.go
+++ go-minio/config.go
@@ -1,10 +1,10 @@
 package gominio
 
 type Config struct {
-	AccessKeyId     string `json:"access_key_id" yaml:"AccessKeyId"`
-	AccessKeySecret string `json:"access_key_secret" yaml:"AccessKeySecret"`
-	Endpoint        string `json:"endpoint" yaml:"Endpoint"`
-	Bucket          string `json:"bucket" yaml:"Bucket"`
-	Domain          string `json:"domain" yaml:"Domain,optional"`
-	UseSSL          bool   `json:"useSSL" yaml:"UseSSL"`
+	AccessKeyId     string `json:",optional" yaml:"AccessKeyId"`
+	AccessKeySecret string `json:",optional" yaml:"AccessKeySecret"`
+	Endpoint        string `json:",optional" yaml:"Endpoint"`
+	Bucket          string `json:",optional" yaml:"Bucket"`
+	Domain          string `json:",optional" yaml:"Domain,optional"`
+	UseSSL          bool   `json:",optional" yaml:"UseSSL"`
 }
