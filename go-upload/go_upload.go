71e2f84f483957753e53ddcb12b6033f72ca983f
姚吉日 <gif@live.cn>
2024-05-16 11:21:03 +0800

    uploadfile func

--- go-upload/go_upload.go
+++ go-upload/go_upload.go
@@ -7,19 +7,23 @@ import (
 	"path/filepath"
 )
 
-// 上传file文件到assetsDir目录下，assetsDir 目录不存在则自动创建
-func UploadFile(assetsDir string, file *multipart.FileHeader) error {
-	name, err := goutils.GetFileHeaderMd5Name(file)
+type FileUploadResult struct {
+	OriginalFile string
+	FileName     string
+}
 
+// 上传file文件到assetsDir目录下，assetsDir 目录不存在则自动创建,返回存储位置
+func UploadFile(assetsDir string, file *multipart.FileHeader) (*FileUploadResult, error) {
+	name, err := goutils.GetFileHeaderMd5Name(file)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	fullName := name + filepath.Ext(file.Filename)
 	if ok, _ := goutils.Exist(assetsDir); !ok {
 		err := goutils.CreateSavePath(assetsDir, os.ModePerm)
 		if err != nil {
-			return err
+			return nil, err
 		}
 	}
 
@@ -27,9 +31,13 @@ func UploadFile(assetsDir string, file *multipart.FileHeader) error {
 	if ok, _ := goutils.Exist(originalPath); !ok {
 		err = goutils.SaveFile(file, originalPath)
 		if err != nil {
-			return err
+			return nil, err
 		}
 	}
 
-	return nil
+	res := &FileUploadResult{}
+	res.OriginalFile = originalPath
+	res.FileName = fullName
+
+	return res, nil
 }
