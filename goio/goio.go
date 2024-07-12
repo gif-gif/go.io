8edeac68f9ed17e7a595fc4d0357aff683f5c550
姚吉日 <gif@live.cn>
2024-07-12 11:27:23 +0800

    op

--- goio/goio.go
+++ goio/goio.go
@@ -1,11 +1,9 @@
 package goio
 
-import golog "github.com/gif-gif/go.io/go-log"
-
 // 当前运行环境
 var Env Environment //string `json:",default=pro,options=dev|test|rt|pre|pro"`
 
 func Init(env Environment) {
 	Env = env
-	golog.SetAdapter(golog.NewFileAdapter()) //默认当前工程目录logs/date.log
+	//golog.SetAdapter(golog.NewFileAdapter()) //默认当前工程目录logs/date.log
 }
