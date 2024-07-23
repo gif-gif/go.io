777d9790df92fd3345cc831edfe5809935e8aec5
姚吉日 <gif@live.cn>
2024-07-23 11:01:20 +0800

    op

--- go-tgbot/types.go
+++ go-tgbot/types.go
@@ -3,14 +3,18 @@ package gotgbot
 import "strconv"
 
 type TelegramBot struct {
-	Product         string `yaml:"Product" json:"product"`
-	Token           string `yaml:"Token" json:"token"`
-	WebAppUrl       string `yaml:"WebAppUrl,optional" json:"webAppUrl,optional"`
-	StartReply      string `yaml:"StartReply" json:"startReply,optional"`
-	Timeout         int64  `yaml:"Timeout,optional" json:"timeout,optional"`                 //s
-	Open            bool   `yaml:"Open,optional" json:"open,optional"`                       //是否开启
-	Secret          string `yaml:"Secret,optional" json:"secret,optional"`                   //请求密钥
-	LinkSignTimeout int64  `yaml:"LinkSignTimeout,optional" json:"linkSignTimeout,optional"` //请求连接过期时间 秒
+	Product    string `yaml:"Product" json:"product"`
+	Token      string `yaml:"Token" json:"token"`
+	WebAppUrl  string `yaml:"WebAppUrl,optional" json:"webAppUrl,optional"`
+	StartReply string `yaml:"StartReply" json:"startReply,optional"`
+	Timeout    int64  `yaml:"Timeout,optional" json:"timeout,optional"` //s
+
+	// Product configuration (可选)
+	Open         bool   `yaml:"Open,optional" json:"open,optional"`                 //是否开启
+	SignSecret   string `yaml:"SignSecret,optional" json:"signSecret,optional"`     //请求签名密钥
+	SignTimeout  int64  `yaml:"SignTimeout,optional" json:"signTimeout,optional"`   //请求签名过期时间-秒
+	AccessSecret string `yaml:"AccessSecret,optional" json:"accessSecret,optional"` //jwt signature secret
+	AccessExpire int64  `yaml:"AccessExpire,optional" json:"accessExpire,optional"` // jwt signature accessExpire
 }
 
 type ChatID int64
