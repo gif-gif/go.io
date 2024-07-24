fc7f0b2f25b69f52f194e69fde49ba22eb1c453a
姚吉日 <gif@live.cn>
2024-07-24 14:19:44 +0800

    changed name

--- go-oss/go-alioss/ali-oss.go
+++ go-oss/go-alioss/ali-oss.go
@@ -2,27 +2,27 @@ package goalioss
 
 import "github.com/aliyun/aliyun-oss-go-sdk/oss"
 
-type GoOss struct {
+type GoAliOss struct {
 	oss *Uploader
 }
 
-func Init(conf Config) *GoOss {
+func Init(conf Config) *GoAliOss {
 	__oss, _ := New(conf)
-	return &GoOss{oss: __oss}
+	return &GoAliOss{oss: __oss}
 }
 
-func (g *GoOss) Client() *oss.Client {
+func (g *GoAliOss) Client() *oss.Client {
 	return g.oss.client
 }
 
-func (g *GoOss) ContentType(value string) *Uploader {
+func (g *GoAliOss) ContentType(value string) *Uploader {
 	return g.oss.ContentType(value)
 }
 
-func (g *GoOss) Options(opts ...oss.Option) *Uploader {
+func (g *GoAliOss) Options(opts ...oss.Option) *Uploader {
 	return g.oss.Options(opts...)
 }
 
-func (g *GoOss) Upload(filename string, body []byte) (string, error) {
+func (g *GoAliOss) Upload(filename string, body []byte) (string, error) {
 	return g.oss.Upload(filename, body)
 }
