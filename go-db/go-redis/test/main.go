5b660a0e56860ec87e77908d6a793ce938db46f6
姚吉日 <gif@live.cn>
2024-07-26 15:27:09 +0800

    redis test case update

--- go-db/go-redis/test/main.go
+++ go-db/go-redis/test/main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-	goredis2 "github.com/gif-gif/go.io/go-db/go-redis"
+	"context"
+	goredis "github.com/gif-gif/go.io/go-db/go-redis"
 	golog "github.com/gif-gif/go.io/go-log"
 	"time"
 )
 
 func main() {
-	config := goredis2.Config{
+	config := goredis.Config{
 		Name:     "goredis",
 		Addr:     "127.0.0.1:6379",
 		Password: "",
@@ -16,15 +17,15 @@ func main() {
 		AutoPing: true,
 	}
 
-	c, err := goredis2.New(config)
+	err := goredis.Init(config)
 	if err != nil {
 		golog.WithTag("goredis").Error(err)
 	}
 
-	cmd := c.Set("goredis", "goredis", time.Duration(10)*time.Second)
+	cmd := goredis.Default().Set(context.Background(), "goredis", "goredis", time.Duration(10)*time.Second)
 	if cmd.Err() != nil {
 		golog.WithTag("goredis").Error(cmd.Err())
 	}
-	v := c.Get("goredis").Val()
+	v := goredis.Default().Get(context.Background(), "goredis").Val()
 	golog.WithTag("goredis").InfoF(v)
 }
