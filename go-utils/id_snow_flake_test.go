171f0b102145e242cf2e7dc058e0551aabc08b26
姚吉日 <gif@live.cn>
2024-09-02 20:57:49 +0800

    op

--- go-utils/id_snow_flake_test.go
+++ go-utils/id_snow_flake_test.go
@@ -6,12 +6,8 @@ import (
 )
 
 func TestGenId(t *testing.T) {
-	//GenIdInit(&SnowFlakeId{machineId: 2})
-	//
-	//for i := 0; i < 5000; i++ {
-	//	fmt.Println(GenId())
-	//}
-
-	aORb := IfNot[[]string](true, []string{"aa"}, []string{})
-	fmt.Print(aORb)
+	GenIdInit(&SnowFlakeId{machineId: 2})
+	for i := 0; i < 5000; i++ {
+		fmt.Println(GenId())
+	}
 }
