c6c9ce3d138eb358bd04fb45f4f68ff3ed13e82d
姚吉日 <gif@live.cn>
2024-09-29 09:53:25 +0800

    add Ts2DateTimeLocal and Ts2DateLocal

--- go-utils/time.go
+++ go-utils/time.go
@@ -7,31 +7,49 @@ func DateTime(format string) string {
 }
 
 func Today() string {
-	return DateTime("2006-01-02")
+	return DateTime(time.DateOnly)
 }
 
 func Now() string {
-	return DateTime("2006-01-02 15:04:05")
+	return DateTime(time.DateTime)
 }
 
 func NextDate(d int) string {
-	return time.Now().AddDate(0, 0, d).Format("2006-01-02")
+	return time.Now().AddDate(0, 0, d).Format(time.DateOnly)
 }
 
 func Ts2Date(ts int64) string {
-	return time.Unix(ts, 0).Format("2006-01-02")
+	return time.Unix(ts, 0).Format(time.DateOnly)
+}
+
+// 本地化日期
+func Ts2DateLocal(ts int64, timeOffsetSec int) string {
+	t := time.Unix(ts, 0)
+	location := time.FixedZone("OffsetZone", timeOffsetSec)
+	// 将时间对象转换为特定时区的时间
+	localTime := t.In(location)
+	return localTime.Format(time.DateOnly)
 }
 
 func Ts2DateTime(ts int64) string {
-	return time.Unix(ts, 0).Format("2006-01-02 15:04:05")
+	return time.Unix(ts, 0).Format(time.DateTime)
+}
+
+// 本地化日期时间
+func Ts2DateTimeLocal(ts int64, timeOffsetSec int) string {
+	t := time.Unix(ts, 0)
+	location := time.FixedZone("OffsetZone", timeOffsetSec)
+	// 将时间对象转换为特定时区的时间
+	localTime := t.In(location)
+	return localTime.Format(time.DateTime)
 }
 
 func Date2Ts(date string) int64 {
-	ti, _ := time.ParseInLocation("2006-01-02", date, time.Local)
+	ti, _ := time.ParseInLocation(time.DateOnly, date, time.Local)
 	return ti.Unix()
 }
 
 func DateTime2Ts(dateTime string) int64 {
-	ti, _ := time.ParseInLocation("2006-01-02 15:04:05", dateTime, time.Local)
+	ti, _ := time.ParseInLocation(time.DateTime, dateTime, time.Local)
 	return ti.Unix()
 }
