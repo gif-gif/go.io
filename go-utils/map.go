08c0ecc8fcd7ad99e3addc29e827c13647fb66b3
姚吉日 <gif@live.cn>
2024-11-27 15:58:35 +0800

    op

---
+++ go-utils/map.go
@@ -0,0 +1,19 @@
+package goutils
+
+import "encoding/json"
+
+type M map[string]interface{}
+
+func (m M) Json() []byte {
+	b, _ := json.Marshal(&m)
+	return b
+}
+
+func (m M) String() string {
+	return string(m.Json())
+}
+
+func (m M) Params() *Params {
+	p, _ := Byte(m.Json()).Params()
+	return p
+}
