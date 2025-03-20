16a792236a51146ddcef8cd00782d8c1ee8e2fe6
姚吉日 <gif@live.cn>
2025-03-20 11:07:22 +0800

    op

--- go-cache/config.go
+++ go-cache/config.go
@@ -1,7 +1,7 @@
 package gocache
 
 type Config struct {
-	Name              string
-	DefaultExpiration int64 //秒
-	CleanupInterval   int64 //秒
+	Name              string `yaml:"Name" json:"name,optional"`
+	DefaultExpiration int64  `yaml:"DefaultExpiration" json:"defaultExpiration,optional"` //秒
+	CleanupInterval   int64  `yaml:"CleanupInterval" json:"cleanupInterval,optional"`     //秒
 }
