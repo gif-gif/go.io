6545f8d0f561be303adc1c5922767602058d7ce8
姚吉日 <gif@live.cn>
2025-07-14 21:47:30 +0800

    errcode bug

--- go-error/errCode.go
+++ go-error/errCode.go
@@ -8,17 +8,19 @@ const OK uint32 = 0
 
 // 全局错误码
 // client
-const REQUEST_PARAM_ERROR uint32 = 1400
-const TOKEN_EXPIRE_ERROR uint32 = 1401
-const FORBIDDEN_ERROR uint32 = 1403
-const NOT_FOUND_ERROR uint32 = 1404
+const REQUEST_PARAM_ERROR uint32 = 400
+const TOKEN_EXPIRE_ERROR uint32 = 401
+const FORBIDDEN_ERROR uint32 = 403
+const NOT_FOUND_ERROR uint32 = 404
+
+const SERVER_COMMON_ERROR uint32 = 1500
 const CAPTCHA_ERROR uint32 = 1700
 const USER_NOT_EXISTS_ERROR uint32 = 1701
 const USER_LOGIN_ERROR uint32 = 1703
 const USER_EXISTS_ERROR uint32 = 1704
 
 // server
-const SERVER_COMMON_ERROR uint32 = 1500
+
 const DB_ERROR uint32 = 1555
 const REDIS_ERROR uint32 = 1666
 const ETCD_ERROR uint32 = 1777
