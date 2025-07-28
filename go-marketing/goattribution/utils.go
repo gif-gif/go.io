2158fe20d9f6f7ee0fa2e0a775908730f71968ea
姚吉日 <gif@live.cn>
2025-07-28 16:07:20 +0800

    get sid when clickId is empty.

--- go-marketing/goattribution/utils.go
+++ go-marketing/goattribution/utils.go
@@ -3,11 +3,18 @@ package goattribution
 import "net/url"
 
 func CreateBaseAttributeInfo(queryParams url.Values, channel string) *AttributeInfo {
+	clickId := queryParams.Get("click_id")
+	if clickId == "" {
+		clickId = queryParams.Get("clickid")
+	}
+	if clickId == "" {
+		clickId = queryParams.Get("sid")
+	}
 	return &AttributeInfo{
 		UtmSource:  queryParams.Get("utm_source"),
 		UtmMedium:  queryParams.Get("utm_medium"),
 		UtmContent: queryParams.Get("utm_content"),
-		ClickId:    queryParams.Get("click_id"),
+		ClickId:    clickId,
 		Channel:    channel,
 	}
 }
