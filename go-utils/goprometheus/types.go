68585e87a5d398d8adf5f9cf477ab2918189e918
姚吉日 <gif@live.cn>
2025-09-13 16:14:52 +0800

    add more types

--- go-utils/goprometheus/types.go
+++ go-utils/goprometheus/types.go
@@ -36,3 +36,32 @@ type MemberLevelUserCount struct {
 	Level int64 `json:"level,optional"` // 会员等级
 	Count int64 `json:"count,optional"` // 用户数
 }
+
+type IpUserCount struct {
+	Ip    string `json:"ip,optional"`    // IP地址
+	Count int64  `json:"count,optional"` // 用户数
+}
+
+type IpBandwidth struct {
+	Ip      string               `json:"ip,optional"`                // IP地址
+	Details []*ProtocolBandwidth `json:"details,optional,omitempty"` // 带宽详情
+}
+
+type IpConnCount struct {
+	Ip      string               `json:"ip,optional"`                // IP地址
+	Details []*ProtocolConnCount `json:"details,optional,omitempty"` // 连接数详情
+}
+
+type ProtocolBandwidth struct {
+	Protocol string `json:"protocol,optional"` // 协议
+	Port     int64  `json:"port,optional"`     // 端口
+	In       int64  `json:"in,optional"`       // 入站带宽
+	Out      int64  `json:"out,optional"`      // 出站带宽
+}
+
+type ProtocolConnCount struct {
+	Protocol string `json:"protocol,optional"` // 协议
+	Port     int64  `json:"port,optional"`     // 端口
+	In       int64  `json:"in,optional"`       // 入站连接数
+	Out      int64  `json:"out,optional"`      // 出站连接数
+}
